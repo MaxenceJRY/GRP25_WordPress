88ef66b1e5ee3870874a700240c29aa5f3d64852
VladimirMichot <vladimirmichot@gmail.com>
2023-09-18 08:53:50 +0700

    view pager notif

--- app/build/generated/data_binding_base_class_source_out/debug/out/vn/edu/usth/wordpress25/databinding/FragmentNotificationsBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/vn/edu/usth/wordpress25/databinding/FragmentNotificationsBinding.java
@@ -10,6 +10,8 @@ import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
 import java.lang.NullPointerException;
 import java.lang.Override;
 import java.lang.String;
@@ -20,12 +22,21 @@ public final class FragmentNotificationsBinding implements ViewBinding {
   private final ConstraintLayout rootView;
 
   @NonNull
+  public final TabLayout tabLayout;
+
+  @NonNull
   public final TextView textNotifications;
 
+  @NonNull
+  public final ViewPager viewPager;
+
   private FragmentNotificationsBinding(@NonNull ConstraintLayout rootView,
-      @NonNull TextView textNotifications) {
+      @NonNull TabLayout tabLayout, @NonNull TextView textNotifications,
+      @NonNull ViewPager viewPager) {
     this.rootView = rootView;
+    this.tabLayout = tabLayout;
     this.textNotifications = textNotifications;
+    this.viewPager = viewPager;
   }
 
   @Override
@@ -55,13 +66,26 @@ public final class FragmentNotificationsBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
+      id = R.id.tabLayout;
+      TabLayout tabLayout = ViewBindings.findChildViewById(rootView, id);
+      if (tabLayout == null) {
+        break missingId;
+      }
+
       id = R.id.text_notifications;
       TextView textNotifications = ViewBindings.findChildViewById(rootView, id);
       if (textNotifications == null) {
         break missingId;
       }
 
-      return new FragmentNotificationsBinding((ConstraintLayout) rootView, textNotifications);
+      id = R.id.viewPager;
+      ViewPager viewPager = ViewBindings.findChildViewById(rootView, id);
+      if (viewPager == null) {
+        break missingId;
+      }
+
+      return new FragmentNotificationsBinding((ConstraintLayout) rootView, tabLayout,
+          textNotifications, viewPager);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
--- app/src/main/java/vn/edu/usth/wordpress25/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/vn/edu/usth/wordpress25/ui/notifications/NotificationsFragment.java
@@ -6,10 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
+import vn.edu.usth.wordpress25.R;
 import vn.edu.usth.wordpress25.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
@@ -26,7 +34,21 @@ public class NotificationsFragment extends Fragment {
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        ViewPager viewPager = root.findViewById(R.id.viewPager);
+        PagerAdapter pagerAdapter = new NotificationsPagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+
+        TabLayout tabLayout = root.findViewById(R.id.tabLayout);
+
+        // Connectez le TabLayout au ViewPager
+        tabLayout.setupWithViewPager(viewPager);
         return root;
+
+
+
     }
 
     @Override
@@ -34,4 +56,62 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+    public class NotificationsPagerAdapter extends FragmentPagerAdapter {
+
+        public NotificationsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            // Retournez le fragment correspondant à la position
+            switch (position) {
+                case 0:
+                    return new NotifAllFragment();
+                case 1:
+                    return new NotifUnreadFragment();
+                case 2:
+                    return new NotifCommentsFragment();
+                case 3:
+                    return new NotifFollowsFragment();
+                case 4:
+                    return new NotifLikesFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Retournez le nombre total d'onglets
+            return 5;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Retournez le titre de l'onglet en fonction de la position
+            switch (position) {
+                case 0:
+                    return "ALL";
+                case 1:
+                    return "UNREAD";
+                case 2:
+                    return "COMMENTS";
+                case 3:
+                    return "FOLLOWS";
+                case 4:
+                    return "LIKES";
+                default:
+                    return null;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
